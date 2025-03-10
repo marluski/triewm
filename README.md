53baeaefc3ff70ade4d7bec030426cb42ad7e64a
mario <mar1lusk1@proton.me>
2025-03-10 10:55:46 +0100

    Initial commit
    Make `cargo` crate, add `main.rs` and `bar.rs`. Also add README.md,
    LICENSE.txt and build.rs

---
+++ README.md
@@ -0,0 +1,29 @@
+# OpenWM
+
+OpenWM is an **extremely minimal**, **open-source** and **free** Window Manager for Linux.
+
+## Features
+
+- Fast
+- Based on Wayland
+- Minimal (Text)
+- Supports graphical applications
+- Supports multiple monitors
+
+## Installation
+
+OpenWM is available in the **`cargo` package manager**.
+
+```bash
+cargo install openwm
+```
+
+## Usage
+
+```bash
+openwm
+```
+
+## License
+
+OpenWM is licensed under the **GNU General Public License v3.0**.
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+/*
+=====================================================================
+=============       OpenWM - Open Window Manager       ===============
+=====================================================================
+
+This is the main file of OpenWM: An open-source, minimal, and customizable Window Manager for Linux.
+*/
+
+// Docs.rs comment thing
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
+
+//! # OpenWM
+//! An **open-source**, minimal, and customizable Window Manager for Linux.
+//!
+//! OpenWM is **extremely lightweight**, and **customizable**. It is written in Rust, and uses the [Smithay](https://github.com/Smithay/smithay) library.
+//!
+//! ## Features
+//! - **Extremly lightweight**: OpenWM can be run on even the lowest-end hardware.
+//! - **Customizable**: OpenWM can be configured to suit your needs.
+//! - **Minimal**: OpenWM is designed to be as minimal as possible.
+//! - **Open-source**: OpenWM is open-source, and can be modified and improved by anyone.
+//!
+//! ## Getting Started
+//! To get started with OpenWM, you can follow the [Getting Started](https://github.com/mariluski/openwm/wiki/Getting-Started) guide on the OpenWM wiki.
+
+use smithay;
+mod bar;
+
+fn main() {}
